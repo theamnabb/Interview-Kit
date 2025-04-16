a739a84f418e9153714d26270ca73cac72b24280
aamna-ansari <aamnansari29@gmail.com>
2025-04-16 20:48:11 +0500

    Refactor callback functions and clean up commented code

--- JavaScript/callback.js
+++ JavaScript/callback.js
@@ -1,6 +1,19 @@
 // Reuse 
-function greet(name){
-    console.log(`Helo:${name}`);    
+// function greet(name){
+//     console.log(`Helo:${name}`);    
+// }
+
+// greet('AaMna')
+
+// 2nd Call back 
+
+function handleUser(callback){
+    const name = 'AaMna'
+    callback(name)
 }
 
-greet('AaMna')
+function getName(name){
+    console.log(`Hello ${name}`)
+
+}
+handleUser(getName)
