865efbed93199e083583f0c724ab672535c192e3
aamna-ansari <aamnansari29@gmail.com>
2025-04-17 21:03:05 +0500

    map

--- JavaScript/callback.js
+++ JavaScript/callback.js
@@ -1,20 +1,25 @@
-// Reuse 
+// Reuse
 // function greet(name){
-//     console.log(`Helo:${name}`);    
+//     console.log(`Helo:${name}`);
 // }
 
 // greet('AaMna')
 
-// 2nd Call back 
+// 2nd Call back
 
-function handleUser(callback){
-    const name = 'AaMna'
-    callback(name)
+function handleUser(callback) {
+  const name = "AaMna";
+  callback(name);
 }
 
-function getName(name){
-    console.log(`Hello ${name}`)
-
+function getName(name) {
+  console.log(`Hello ${name}`);
 }
-handleUser(getName)
+handleUser(getName);
 
+// Perform High Order Function
+
+let arr = [1, 2, 3, 4, 5, 6];
+console.log(length(arr));
+
+let doulbleArry = arr.map((num) => num * 2);
