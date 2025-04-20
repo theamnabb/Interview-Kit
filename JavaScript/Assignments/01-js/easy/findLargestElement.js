2eed6dfe6ce27899c523f2a66ab9e608d310a63e
aamna-ansari <aamnansari29@gmail.com>
2025-04-20 17:30:17 +0500

    findLargestElement function to use reduce

--- JavaScript/Assignments/01-js/easy/findLargestElement.js
+++ JavaScript/Assignments/01-js/easy/findLargestElement.js
@@ -5,12 +5,23 @@
   - Output: 9
 */
 
-function findLargestElement(numbers) {
+// function findLargestElement(numbers) {
 
-    let arrNumSort = numbers.sort();
-    let findLargest = Math.max(...arrNumSort)
-    console.log(`The largest element is: ${findLargest}`);
-}
+//     let arrNumSort = numbers.sort();
+//     let findLargest = Math.max(...arrNumSort)
+//     console.log(`The largest element is: ${findLargest}`);
+// }
+// findLargestElement([3, 7, 2, 9, 1]); // Output: 9
+
+// 2nd method by using reduce
+function findLargestElement(numbers) {
+    let max = numbers.reduce((acc, curr) => {
+      return acc > curr ? acc : curr;
+    });
+  
+    console.log(`The largest element is: ${max}`);
+    return max;
+  }
 findLargestElement([3, 7, 2, 9, 1]); // Output: 9
 
 module.exports = findLargestElement;
