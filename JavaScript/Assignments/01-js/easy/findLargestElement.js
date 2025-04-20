3bc3873c4fd6ef67df924aa169fecf6b8222c20f
aamna-ansari <aamnansari29@gmail.com>
2025-04-20 17:22:19 +0500

    Refactor findLargestElement function to correctly find and log the largest number from an array

--- JavaScript/Assignments/01-js/easy/findLargestElement.js
+++ JavaScript/Assignments/01-js/easy/findLargestElement.js
@@ -6,7 +6,11 @@
 */
 
 function findLargestElement(numbers) {
-    
+
+    let arrNumSort = numbers.sort();
+    let findLargest = Math.max(...arrNumSort)
+    console.log(`The largest element is: ${findLargest}`);
 }
+findLargestElement([3, 7, 2, 9, 1]); // Output: 9
 
 module.exports = findLargestElement;
