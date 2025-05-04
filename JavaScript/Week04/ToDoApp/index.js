a6a8d64fd50ede811e0a906dfcc83f1e0a6b3358
aamna-ansari <aamnansari29@gmail.com>
2025-05-04 18:53:02 +0500

    fix: correct stylesheet link and enhance task addition functionality

--- JavaScript/Week04/ToDoApp/index.js
+++ JavaScript/Week04/ToDoApp/index.js
@@ -1,2 +1,30 @@
  let taskInput = document.getElementById('taskInput');
  let addTaskButton = document.getElementById('addTaskButton');
+ let itemsList = document.getElementById('itemsList');
+
+// add event listener to the button
+
+addTaskButton.addEventListener('click', ()=>{
+    let getInputValue = taskInput.value;
+    if (getInputValue === ''){
+        alert('Please enter a task!');
+    }
+// Add newToDo 
+
+let newToDo = document.createElement('li');
+newToDo.innerHTML = `<div class="task-content">
+              <input type="checkbox" class="checkbox" id="task1">
+              <span>I'm AaMna</span>
+            </div>
+            <button class="deleteButton">
+              <i class="fa-solid fa-trash"></i>
+            </button>
+`
+
+itemsList.appendChild(newToDo);
+// Clear input field after adding todo
+taskInput.value = '';
+})
+
+
+
