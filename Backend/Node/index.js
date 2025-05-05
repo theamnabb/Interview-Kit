92b231c67830b763981710b9ca0a62b051d17e19
aamna-ansari <aamnansari29@gmail.com>
2025-05-05 22:41:21 +0500

    added

--- Backend/Node/index.js
+++ Backend/Node/index.js
@@ -2,4 +2,4 @@ console.log("Hello from Node.js");
 console.log("Hello from Node.js by using VsCode terminal");
 console.log("Hello from Node.js by using nodemon command");
 
-console.log("Yahooooo i have successsfully install ");
+console.log("Yahooooo i have successsfully install nodemon for just development ");
