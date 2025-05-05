dc21fbeea95223fc246d395687120d7f3ff04fe8
aamna-ansari <aamnansari29@gmail.com>
2025-05-05 22:41:37 +0500

    nodemon install

--- Backend/Node/index.js
+++ Backend/Node/index.js
@@ -1,5 +1,2 @@
 console.log("Hello from Node.js");
-console.log("Hello from Node.js by using VsCode terminal");
-console.log("Hello from Node.js by using nodemon command");
 
-console.log("Yahooooo i have successsfully install nodemon for just development ");
