5e9441d23f72c339f0fe380dec776500ede9414c
aamna-ansari <aamnansari29@gmail.com>
2025-05-07 12:50:37 +0500

    feat: enhance Enquiry component with a new enquiry list table and improved layout

--- Project/frontend/src/Compontents/Enquiry.jsx
+++ Project/frontend/src/Compontents/Enquiry.jsx
@@ -1,17 +1,28 @@
 import React from "react";
-import { Button, Textarea, Label, TextInput } from "flowbite-react";
+import {
+  Button,
+  Textarea,
+  Label,
+  TextInput,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeadCell,
+  TableRow,
+} from "flowbite-react";
 
 const Enquiry = () => {
-    let saveEnquiry = (e)=>{
-        alert("Enquiry Saved Successfully")
-        e.preventDefault();
-    }
+  let saveEnquiry = (e) => {
+    alert("Enquiry Saved Successfully");
+    e.preventDefault();
+  };
   return (
     <div>
       <h1 className="text-[40px] text-center py-6 font-bold ">User Enquiry</h1>
-      <div className="grid grid-cols-[30%_auto]">
+      <div className="grid grid-cols-[30%_auto] gap-9">
         {/* Form Side  */}
-        <div className="bg-blue-900 p-4">
+        <div className="bg-blue-900 p-4 rounded-2xl">
           <h2 className="text-[20px] font-bold text-amber-300">Enquiry Form</h2>
           <form action="" onSubmit={saveEnquiry}>
             <div className="p-3">
@@ -66,7 +77,54 @@ const Enquiry = () => {
             </div>
           </form>
         </div>
+
         {/* Table side  */}
+        <div className="bg-blue-900 p-4 rounded-2xl">
+          <h2 className="text-[20px] font-bold text-amber-300 ">Enquiry List</h2>
+          <div className="overflow-x-auto ">
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableHeadCell>Sr.No</TableHeadCell>
+            <TableHeadCell>Name</TableHeadCell>
+            <TableHeadCell>Email</TableHeadCell>
+            <TableHeadCell>Phone</TableHeadCell>
+            <TableHeadCell>Message</TableHeadCell>
+            <TableHeadCell>Delete</TableHeadCell>
+            <TableHeadCell>Edit</TableHeadCell>
+            <TableHeadCell>
+              <span className="sr-only">Edit</span>
+            </TableHeadCell>
+            <TableHeadCell>
+              <span className="sr-only">Delete</span>
+            </TableHeadCell>
+          </TableRow>
+        </TableHead>
+        <TableBody className="divide-y">
+          <TableRow className="">
+            <TableCell className="whitespace-nowrap font-medium  ">
+              01
+            </TableCell>
+            <TableCell>Sliver</TableCell>
+            <TableCell>Laptop</TableCell>
+            <TableCell>$2999</TableCell>
+            <TableCell>Hello Word</TableCell>
+            <TableCell>
+              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                Delete
+              </a>
+            </TableCell>
+            <TableCell>
+              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                Edit
+              </a>
+            </TableCell>
+          </TableRow>
+          
+        </TableBody>
+      </Table>
+    </div>
+        </div>
       </div>
     </div>
   );
