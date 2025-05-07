a032ac84354d28f3c9a62e75a56eba059787bc5d
aamna-ansari <aamnansari29@gmail.com>
2025-05-07 09:19:42 +0500

    routing and server setup in index.js

--- Backend/Node/index.js
+++ Backend/Node/index.js
@@ -3,3 +3,24 @@
 
 
  app.use(express.json()) // Middleware to parse JSON bodies
+
+//  Rounting with Express
+
+// First one 
+app.get('/students-read',(req, res)=>{
+    res.send('Helo From student read')
+})
+
+// Second Post 
+
+app.post('/students-create',(req, res)=>{
+    res.send("Helo from student Create ");
+    
+})
+
+// Sever running on port 8000
+const PORT = 8000; // Port number
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
