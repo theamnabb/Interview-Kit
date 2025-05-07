398f435be333d8bd3a6f6b024545bb47e5d21ff3
aamna-ansari <aamnansari29@gmail.com>
2025-05-07 10:55:36 +0500

    enhance MongoDB integration and update README with new learnings

--- Backend/Node/index.js
+++ Backend/Node/index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose"); // Importing mongoose for MongoDB connecti
 const app = express();
 
 app.use(express.json()); // Middleware to parse JSON bodies
+// Middleware
+app.use(express.urlencoded({ extended: false }));
 
 //  Connect MongoDB
 mongoose
@@ -13,6 +15,25 @@ mongoose
   .catch((err) => {
     console.log("Error from MongoDB connection", err);
   });
+
+//   create a Schema
+
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+},{timestamps:true});
+
+// Use this schema for model | model name always captial first letter
+
+const Students = mongoose.model("students", studentSchema);
+
 //  Rounting with Express
 
 // First one
@@ -22,8 +43,19 @@ app.get("/students-read", (req, res) => {
 
 // Second Post
 
-app.post("/students-create", (req, res) => {
-  res.send("Helo from student Create ");
+app.post("/students-create", async (req, res) => {
+  //   res.send("Helo from student Create ");
+  const body = req.body;
+  if (!body || !body.name || !body.email) {
+    return res.status(400).json({ msg: "All field are required...." });
+  }
+
+  const result = await Students.create({
+    name: body.name,
+    email: body.email,
+  });
+  console.log("result", result);
+  return res.status(201).json({ msg: "success" });
 });
 
 // Sever running on port 8000
