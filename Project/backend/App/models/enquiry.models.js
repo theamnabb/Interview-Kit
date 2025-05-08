2159078907af8ec27419467c2e4c643ff4d20eee
aamna-ansari <aamnansari29@gmail.com>
2025-05-08 15:16:53 +0500

    add user enquiry model with schema definition

---
+++ Project/backend/App/models/enquiry.models.js
@@ -0,0 +1,27 @@
+let mongoose = require("mongoose");
+
+let userEnquirySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+},{timestamps: true});
+
+// Use in model 
+
+const UserEnquiryModel = mongoose.model("UserEnquiry", userEnquirySchema);
+
+module.exports = UserEnquiryModel;
