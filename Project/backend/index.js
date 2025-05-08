276d08999ac2fc9c1d8a3e5a29581c218ccaeab8
aamna-ansari <aamnansari29@gmail.com>
2025-05-08 14:51:44 +0500

    mongoDB connect

--- Project/backend/index.js
+++ Project/backend/index.js
@@ -1,5 +1,21 @@
-
 // Express server setup
 
-let express = require('express');
+let express = require("express");
 let app = express();
+let mongoose = require("mongoose");
+require("dotenv").config();
+
+// conect moogose
+
+mongoose.connect
+  .connect(process.env.DBRUL)
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log("error connecting to DB", err);
+  });
+
+app.listen(process.env.PORT || 3000, () => {
+  log("server is running on port");
+});
