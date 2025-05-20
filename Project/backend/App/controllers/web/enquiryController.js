038c34c6929fe330f79f0973029261bd059ab01c
aamna-ansari <aamnansari29@gmail.com>
2025-05-20 20:54:21 +0500

    delete

--- Project/backend/App/controllers/web/enquiryController.js
+++ Project/backend/App/controllers/web/enquiryController.js
@@ -4,7 +4,8 @@ let enquiryInsert = (req, res) => {
   let { name, email, phone, message } = req.body;
   let enquiry = new UserEnquiryModel({ name, email, phone, message });
 
-  enquiry.save()
+  enquiry
+    .save()
     .then((result) => {
       // console.log("Enquiry Inserted", result);
       res.status(201).json({ message: "Enquiry Inserted", enquiry: result });
@@ -15,10 +16,19 @@ let enquiryInsert = (req, res) => {
     });
 };
 
-let enquiryList = async (req, res) =>{
-  let enquiry = await UserEnquiryModel.find()
-    res.send({status:1, enquiryList: enquiry})
-}
+let enquiryList = async (req, res) => {
+  let enquiry = await UserEnquiryModel.find();
+  res.send({ status: 1, enquiryList: enquiry });
+};
 
+// For Delete
 
-module.exports = {enquiryInsert, enquiryList}
+let enquiryDelete = async (req, res) => {
+  let enquiryId = req.params.id;
+  let enquiry = await UserEnquiryModel.deleteOne({ _id: enquiryId });
+  if (enquiry.deletedCount > 0) {
+    res.send({ status: 1, message: "Enquiry Deleted" });
+  }
+};
+
+module.exports = { enquiryInsert, enquiryList, enquiryDelete };
